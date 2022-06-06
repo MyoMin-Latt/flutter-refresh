bc9b110ccf84781f4647574a415050ccca291ef7
Myo Min Latt <myominlatt.sbs@gmail.com>
2022-06-06 10:38:42 +0630

    SmartRefresher

---
+++ lib/refresh_page.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+class RefreshPage extends StatefulWidget {
+  const RefreshPage({Key? key}) : super(key: key);
+
+  @override
+  State<RefreshPage> createState() => _RefreshPageState();
+}
+
+class _RefreshPageState extends State<RefreshPage> {
+  List<String> items = ["1", "2", "3", "4", "5", "6", "7", "8"];
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    items.add((items.length + 1).toString());
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pull To Refresh'),
+      ),
+      body: SmartRefresher(
+        enablePullDown: true,
+        enablePullUp: true,
+        header: const WaterDropHeader(),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: ListView.builder(
+          itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
+          itemExtent: 100.0,
+          itemCount: items.length,
+        ),
+      ),
+    );
+  }
+
+  // from 1.5.0, it is not necessary to add this line
+  //@override
+  // void dispose() {
+  //  _refreshController.dispose();
+  //  super.dispose();
+  // }
+}
